f00caf4f106335eddabd7ba3ddcca9b75c81e28f
seodongseong <ehdtjd0821@naver.com>
2025-10-22 15:17:29 +0900

    aaaa

---
+++ index.js
@@ -0,0 +1,18 @@
+// ------------------------------------
+// 01
+// ------------------------------------
+
+// const _ = require("lodash")
+
+// const arr = [1,2,3,4,5];
+// const shuffled = _.shuffle(arr);
+// console.log("Shuffled:", shuffled);
+// console.log("HELLO WORLD!!")
+
+// ------------------------------------
+// 02
+// ------------------------------------
+import _ from 'lodash'
+const arr = [1,2,3,4,5];
+const shuffled = _.shuffle(arr);
+console.log("Shuffled_!!!:", shuffled);
